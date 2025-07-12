0bba787d4ce98e869572cade71eae1801e9ebc5b
Git Tutorial <arpita1207-max>
2025-07-12 22:59:06 +0530

    made changes in cicd yml

--- my_dbt_project/snapshots/snap_tags.sql
+++ my_dbt_project/snapshots/snap_tags.sql
@@ -1,22 +1,20 @@
-{%snapshot snap_tags %}
+{% snapshot snap_tags %}
 {{
     config(
         target_schema='snapshots',
-        unique_key=['user_id','movie_id','tag'],
+        unique_key=["user_id", "movie_id", "tag"],
         strategy='timestamp',
         updated_at='tag_timestamp',
         invalidate_hard_deletes=True
-
     )
 }}
 
 select 
-{{dbt_utils.generate_surrogate_key(['user_id','movie_id','tag'])}} as row_key,
-user_id,
-movie_id,
-tag,
-cast(tag_timestamp as timestamp_ntz) as tag_timestamp
-from {{ref('src_tags')}}
-
+  {{ dbt_utils.generate_surrogate_key(['user_id','movie_id','tag']) }} as row_key,
+  user_id,
+  movie_id,
+  tag,
+  cast(tag_timestamp as timestamp_ntz) as tag_timestamp
+from {{ ref('src_tags') }}
 
 {% endsnapshot %}
